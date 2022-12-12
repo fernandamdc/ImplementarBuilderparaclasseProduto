cd9da96e40bb05c85abe3099f56c20bf9a0bc0c0
Fernanda Costa <89755630+fernandamdc@users.noreply.github.com>
2022-12-12 08:52:21 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,153 @@
+import java.time.LocalDate;
+
+public class Produto {
+    private long id;
+    private String titulo;
+    private String descricao;
+    private String marca;
+    private String modelo;
+    private int estoque = 0;
+    private double preco;
+    private LocalDate dataCadastro;
+    private LocalDate dataUltimaAtualizacao;
+    private String urlFoto;
+    private String categoria;
+    private double peso;
+    private double altura;
+    private double largura;
+    private double profundidade;
+
+    public Produto(String titulo, double preco, LocalDate dataCadastro, LocalDate dataUltimaAtualizacao, String categoria) {
+        this.titulo = titulo;
+        this.preco = preco;
+        this.dataCadastro = dataCadastro;
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+        this.categoria = categoria;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco <= 0){
+
+        }else{
+            this.preco = preco;
+        }
+
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+        
+    }
+
+    public LocalDate getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+
+    public void setDataUltimaAtualizacao(LocalDate dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getProfundidade() {
+        return profundidade;
+    }
+
+    public void setProfundidade(double profundidade) {
+        this.profundidade = profundidade;
+    }
+}
